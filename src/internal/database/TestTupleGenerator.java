1b838d5b422ff09c53a7aaa4ce15d04720e08d12
arash.khoshparvar <arash.khoshparvar@6f944a3c-2108-3bfe-791b-3505dc1652ce>
2014-04-07 04:50:15 +0000

    P4 Test middle

--- src/internal/database/TestTupleGenerator.java
+++ src/internal/database/TestTupleGenerator.java
@@ -8,6 +8,16 @@ package internal.database;
 
 import static java.lang.System.out;
 
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.apache.commons.math3.stat.descriptive.moment.Mean;
+import org.apache.commons.math3.stat.descriptive.moment.StandardDeviation;
+
+import com.google.common.primitives.Doubles;
+
 /*******************************************************************************
  * This class tests the TupleGenerator on the Student Registration Database
  * defined in the Kifer, Bernstein and Lewis 2006 database textbook (see figure
@@ -35,7 +45,7 @@ public class TestTupleGenerator {
 				"String String String String", "crsCode", null);
 
 		test.addRelSchema("Teaching", "crsCode semester profId",
-				"String String Integer", "crcCode semester", new String[][] {
+				"String String Integer", "crsCode semester", new String[][] {
 						{ "profId", "Professor", "id" },
 						{ "crsCode", "Course", "crsCode" } });
 
@@ -51,11 +61,85 @@ public class TestTupleGenerator {
 		String[] tables = { "Student", "Professor", "Course", "Transcript",
 				"Teaching" };
 
-		int tups[] = new int[] { 10000, 1000, 2000, 50000, 5000 };
+		int tups[] = new int[] { 1000000, 1000, 2000, 50000, 5000 };
 
 		Comparable[][][] resultTest = test.generate(tups);
 
-		for (int i = 0; i < resultTest.length; i++) {
+		Table student = new Table("Student", "id name address status",
+				"Integer String String String", "id");
+		
+		Table prof = new Table("Professor", "id name deptId",
+				"Integer String String", "id");
+		
+		Table course = new Table("Course", "crsCode deptId crsName descr",
+				"String String String String", "crsCode");
+		
+		Table trans = new Table("Transcript", "studId crsCode semester grade", 
+				"Integer String String String", "studId crsCode semester");
+		
+		Table teaching = new Table("Teaching", "crsCode semester profId",
+				"String String Integer", "crsCode semester");
+		
+		for (int i = 0; i < resultTest.length; i++){
+			switch (i) {
+			case 0: //Student
+				for (int j = 0; j < resultTest[i].length; j++){
+					student.insert(resultTest[i][j]);
+				}
+				break;
+			case 1: //Prof
+				for (int j = 0; j < resultTest[i].length; j++){
+					prof.insert(resultTest[i][j]);
+				}
+				break;
+			case 2: //Course
+				for (int j = 0; j < resultTest[i].length; j++){
+					course.insert(resultTest[i][j]);
+				}
+				break;
+			case 3: //Trans.
+				for (int j = 0; j < resultTest[i].length; j++){
+					trans.insert(resultTest[i][j]);
+				}
+				break;
+			case 4: //Teaching
+				for (int j = 0; j < resultTest[i].length; j++){
+					teaching.insert(resultTest[i][j]);
+				}
+				break;
+
+			default:
+				break;
+			}
+		}
+
+		List<Double> resutls = new ArrayList<>();
+		
+		for (int i = 0; i < 100; i++){
+			long beforeTime = System.nanoTime();
+			
+			Random rand = new Random();
+			int id = rand.nextInt(1000000);
+			
+			student.select("id == " + id);
+			
+			long afterTime = System.nanoTime();
+			
+//			System.out.println(id + " ||| " + (afterTime - beforeTime) / 10E6);
+			resutls.add((afterTime - beforeTime) / 10E6);
+		}
+		
+		StandardDeviation stdDev = new StandardDeviation();
+		Mean mean = new Mean();
+		resutls.remove(0);
+		
+		double stdErr = stdDev.evaluate(Doubles.toArray(resutls)) / Math.sqrt(resutls.size());
+		double average = mean.evaluate(Doubles.toArray(resutls));
+		
+		System.out.println("Std Err: " + stdErr);
+		System.out.println("Average: " + average);
+		
+		/*for (int i = 0; i < resultTest.length; i++) {
 			out.println(tables[i]);
 			for (int j = 0; j < resultTest[i].length; j++) {
 				for (int k = 0; k < resultTest[i][j].length; k++) {
@@ -65,6 +149,6 @@ public class TestTupleGenerator {
 			} // for
 			out.println();
 		} // for
-	} // main
+*/	} // main
 
 } // TestTupleGenerator
